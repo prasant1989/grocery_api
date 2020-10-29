92f8eef6f0d3d84d40583e64e4a5c34636255b36
Prasant <mr.pkmohanty@gmail.com>
2020-10-29 16:20:35 +0530

    Added Validations

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,2 +1,9 @@
 class Catalog < ApplicationRecord
+  before_validation(on: :create) do
+    self.unit = unit.upcase if attribute_present?("unit")
+  end
+  UNIT_TYPES = ['KG', 'L', 'UNIT']
+  validates :unit , presence: true, inclusion: { in: UNIT_TYPES}
+  validates :name , presence: true
+  validates :name , uniqueness: true
 end
